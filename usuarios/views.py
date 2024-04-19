9cd9e0eb47a44560367f05448ca25973ba9342a1
Diego <diegoleal.ma@gmail.com>
2024-04-19 00:18:37 -0300

    Começando cadastro dos Médicos

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 
@@ -17,13 +18,16 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
         users = User.objects.filter(username=username)
         if users.exists():
-            print('Erro 1')
+            messages.add_message(request, constants.ERROR,
+                                 'O usuário já existe')
             return redirect('/usuarios/cadastro')
         if senha != confirmar_senha:
-            print('Erro ')
+            messages.add_message(request, constants.ERROR,
+                                 'A senha e confirmar senha devem ser iguais')
             return redirect('/usuarios/cadastro')
         if len(senha) < 6:
-            print('Erro 3')
+            messages.add_message(request, constants.ERROR,
+                                 'A senha deve possuir pelo menos 6 caracteres')
             return redirect('/usuarios/cadastro')
 
         try:
@@ -36,3 +40,25 @@ def cadastro(request):
         except:
             print('Erro 4')
             return redirect('/usuarios/cadastro')
+
+
+def login_view(request):
+    if request.method == "GET":
+        print(request.user)
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+        user = auth.authenticate(request, username=username, password=senha)
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        messages.add_message(request, constants.ERROR,
+                             'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+
+
+def sair(request):
+    print(request.user.email)
+    auth.logout(request)
+    return redirect('/usuarios/login')
